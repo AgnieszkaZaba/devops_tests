11c09115b55b85510127c813576737a7abfbf334
Agnieszka Żaba <azaba@agh.edu.pl>
2025-12-21 08:17:58 +0100

    update tests; error msg

--- hooks/notebooks_output.py
+++ hooks/notebooks_output.py
@@ -15,22 +15,29 @@ class NotebookTestError(Exception):
 
 def test_cell_contains_output(notebook):
     """checks if all notebook cells have an output present"""
-    for cell in notebook.cells:
+    for cell_idx, cell in enumerate(notebook.cells):
         if cell.cell_type == "code" and cell.source != "":
             if cell.execution_count is None:
-                raise ValueError("Cell does not contain output")
+                raise ValueError(f"Cell {cell_idx} does not contain output")
 
 
 def test_no_errors_or_warnings_in_output(notebook):
     """checks if all example Jupyter notebooks have clear std-err output
     (i.e., no errors or warnings) visible; except acceptable
     diagnostics from the joblib package"""
-    for cell in notebook.cells:
+    for cell_idx, cell in enumerate(notebook.cells):
         if cell.cell_type == "code":
             for output in cell.outputs:
-                if "name" in output and output["name"] == "stderr":
-                    if not output["text"].startswith("[Parallel(n_jobs="):
-                        raise ValueError(output["text"])
+                ot = output.get("output_type")
+                if ot == "error":
+                    raise ValueError(
+                        f"Cell [{cell_idx}] contain error or warning. \n\n"
+                        f"Cell [{cell_idx}] output:\n{output}\n"
+                    )
+                if ot == "stream" and output.get("name") == "stderr":
+                    out_text = output.get("text")
+                    if out_text and not out_text.startswith("[Parallel(n_jobs="):
+                        raise ValueError(f" Cell [{cell_idx}]: {out_text}")
 
 
 def main(argv: Sequence[str] | None = None) -> int:
@@ -41,7 +48,7 @@ def main(argv: Sequence[str] | None = None) -> int:
 
     retval = 0
     test_functions = [
-        # test_cell_contains_output,
+        test_cell_contains_output,
         test_no_errors_or_warnings_in_output,
     ]
     for filename in args.filenames:
